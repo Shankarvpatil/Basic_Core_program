a5facf233c35125c2801600eca423a6279853c4e
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-24 21:40:48 +0530

    Update Harmonic.java

--- Harmonic.java
+++ Harmonic.java
@@ -1,18 +1,32 @@
 import java.util.Scanner;
-public class HarmonicSeries {
+public class LargestNumber {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter a number : ");
-        int num = s.nextInt();
-        double harmonicNum = 0;
+        System.out.print("Enter the size of an array: ");
+        int arraySize = s.nextInt();
+        int[] arr = arrayCreation(arraySize);
+        int largestNum = findMax(arraySize, arr);
+        System.out.println("Largest number is " + largestNum);
+    }
 
-        if (num>0) {
-            for (int i=1; i<=num; i++) {
-                harmonicNum = harmonicNum + (1.0/i);
-            }
-            System.out.printf("Harmonic number is: %.4f ", harmonicNum);
-        } else {
-            System.out.println("Number should be greater than 0.");
+    private static int[] arrayCreation(int arraySize){
+        Scanner s = new Scanner(System.in);
+        int[] number = new int[arraySize];
+        for ( int i=0; i<arraySize; i++ ) {
+            System.out.print("Enter value: ");
+            number[i] = s.nextInt();
         }
+        return number;
+    }
+
+    // 'findMax' function return a Largest values from an array.
+    private static int findMax(int arraySize, int[] arrElements) {
+        int largestNum = Integer.MIN_VALUE;
+        for (int i=0; i<arraySize; i++) {
+            if (largestNum < arrElements[i]) {
+                largestNum = arrElements[i];
+            }
+        }
+        return largestNum;
     }
 }
